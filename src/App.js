e1c152d16895b2481846ab0e37d99fa67c5ec899
Cedric Mendoza <cedric.mendoza02@gmail.com>
2022-06-02 00:18:39 +0800

    Timer component now owns the timer interval

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import { TimerControl, Timer } from './components';
 import './App.css';
 
 function App() {
-  const [currentTimer, setCurrentTimer] = useState()
   const [workMinutes, setWorkMinutes] = useState(25)
   const [breakMinutes, setBreakMinutes] = useState(5)
-  const [activeTimer, setActiveTimer] = useState("work")
-  const [seconds, setSeconds] = useState(0)
+  const [isRunning, setIsRunning] = useState(false)
 
   const increase = (update) => {
     update(prevState => prevState < 60 ? prevState + 1 : prevState)
@@ -18,7 +16,7 @@ function App() {
   }
 
   const startTimer = () => {
-      return
+      setIsRunning(prevState => !prevState)
   }
 
   return (
@@ -38,10 +36,10 @@ function App() {
         />
       <div>
         <Timer 
-          activeTimer={activeTimer === 'work' ? 
-                        {title: "Work", minutes: workMinutes} : 
-                        {title: "Break", minutes: breakMinutes}} 
-          seconds={seconds}/>
+          workMinutes={workMinutes}
+          breakMinutes={breakMinutes}
+          isRunning={isRunning}/>
+          
           <button onClick={startTimer}>Start/Pause</button>
       </div>
     </div>
--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import useInterval from '../../custom_hooks/useInterval'
 
-const Timer = ({ activeTimer, seconds }) => {
-  let title = activeTimer.title;
-  let mm = activeTimer.minutes;
-  let ss = seconds < 10 ? `0${seconds}` : `${seconds}`;
+const Timer = ({workMinutes, breakMinutes, isRunning}) => {
+  const [minutes, setMinutes] = useState(workMinutes + breakMinutes)
+  const [seconds, setSeconds] = useState(0)
+
+  useEffect(() => {
+    setMinutes(workMinutes + breakMinutes)
+    setSeconds(0)
+  }, [workMinutes, breakMinutes])
+
+  useInterval(() => {
+    if(seconds === 0) {
+     setMinutes(prevState => prevState - 1)
+     setSeconds(59)
+     return
+    }
+    setSeconds(prevState => prevState - 1)
+  }, isRunning ? 10 : null)
+
+  let mm = minutes < 10 ? `0${minutes}` : minutes
+  let ss = seconds < 10 ? `0${seconds}` : seconds
 
   return (
     <div>
-        <h2>{title}</h2>
+        <h2>{minutes > breakMinutes ? 'Work' : 'Break'}</h2>
         <p>{mm}:{ss}</p>
     </div>
   )
